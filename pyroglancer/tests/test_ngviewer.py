c17040b683f07d16f74e228a9665199b7eb58753
Sri <j.sridharrajan@gmail.com>
2020-11-25 21:09:33 +0000

    Updated testcases of ngviewer.

---
+++ pyroglancer/tests/test_ngviewer.py
@@ -0,0 +1,18 @@
+import neuroglancer as ng
+from ..ngviewer import openviewer
+
+def test_newviewer():
+    # check if the viewer opens when no object is passed....
+
+    viewer = openviewer(None)
+
+    assert isinstance(viewer, ng.viewer.Viewer)
+
+
+def test_reuseoldviewer():
+    # check if old viewer is reused incase of new opening request....
+
+    viewer1 = openviewer(None)
+    viewer2 = openviewer(viewer1)
+
+    assert viewer1 == viewer2
