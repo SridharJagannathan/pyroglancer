f561291572425d4e21aaea505bff890bd1515a12
Sri <j.sridharrajan@gmail.com>
2021-01-14 12:53:49 +0000

    Adding labels for meshes.

--- pyroglancer/volumes.py
+++ pyroglancer/volumes.py
@@ -101,6 +101,8 @@ def uploadmeshes(volumedatasource, volumeidlist, volumenamelist, path):
     cv = CloudVolume(path, info=info)
 
     cv.mesh.meta.info['@type'] = 'neuroglancer_legacy_mesh'
+    cv.mesh.meta.info['segment_name_map'] = 'segment_names'
+    cv.mesh.meta.info['segment_properties'] = 'segment_properties'
     cv.mesh.meta.commit_info()
 
     files = [os.path.join(cv.mesh.meta.mesh_path, str(vol.segid)) for vol in volumedatasource]
@@ -108,6 +110,7 @@ def uploadmeshes(volumedatasource, volumeidlist, volumenamelist, path):
 
     for fileidx in range(len(files)):
         fullfilepath = str(files[fileidx])  # files[fileidx]
+        print(files[fileidx])
         fullfilepath = os.path.join(cv.basepath, os.path.basename(path), fullfilepath)
         uploadvol = Mesh(
             vertices=volumedatasource[fileidx].vertices, faces=volumedatasource[fileidx].faces,
@@ -124,3 +127,33 @@ def uploadmeshes(volumedatasource, volumeidlist, volumenamelist, path):
         manifestfilepath = os.path.join(cv.basepath, os.path.basename(path), manifestfilepath)
         with open(manifestfilepath, 'w') as f:
             json.dump(manifestinfo, f)
+
+    # create the file for segment_properties
+    allvolproplist = {"id": "label",
+                      "type": "label",
+                      "values": volumenamelist}
+
+    volinfo = {"@type": "neuroglancer_segment_properties",
+               "inline": {"ids": list(map(str, volumeidlist)),
+                          "properties": [allvolproplist]}}
+    volfilepath = os.path.join(cv.basepath, os.path.basename(path), os.path.join(cv.mesh.meta.mesh_path),
+                               'segment_properties')
+    if not os.path.exists(volfilepath):
+        os.makedirs(volfilepath)
+        print('creating:', volfilepath)
+    volinfofile = os.path.join(volfilepath, 'info')
+    with open(volinfofile, 'w') as volinfofile:
+        json.dump(volinfo, volinfofile)
+
+    # create the file for segment_names
+    volumenamedict = dict(zip(map(str, volumeidlist), volumenamelist))
+    volnamemap = {"@type": "neuroglancer_segment_name_map",
+                  "map": volumenamedict}
+    volnamefilepath = os.path.join(cv.basepath, os.path.basename(path), os.path.join(cv.mesh.meta.mesh_path),
+                                   'segment_names')
+    if not os.path.exists(volnamefilepath):
+        os.makedirs(volnamefilepath)
+        print('creating:', volnamefilepath)
+    volnamemapfile = os.path.join(volnamefilepath, 'info')
+    with open(volnamemapfile, 'w') as volnamemapfile:
+        json.dump(volnamemap, volnamemapfile)
