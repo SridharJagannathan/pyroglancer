b35a08fbddc9dba11a3eaca23c9777cd1d8072fc
Sri <j.sridharrajan@gmail.com>
2021-01-16 22:07:38 +0000

    Updated from rst to md file.

---
+++ README.md
@@ -0,0 +1,42 @@
+![image](https://www.travis-ci.com/SridharJagannathan/pyroglancer.svg?branch=master%0A%20:alt:%20Build%20status%0A%20:target:%20https://www.travis-ci.com/SridharJagannathan/pyroglancer)
+
+![image](https://coveralls.io/repos/github/SridharJagannathan/pyroglancer/badge.svg?branch=master%0A%20:alt:%20Code%20coverage%0A%20:target:%20https://coveralls.io/github/SridharJagannathan/pyroglancer?branch=master)
+
+![image](https://img.shields.io/pypi/v/pyroglancer.svg%0A%20%20%20%20%20:alt:%20pypi%20version%0A%20%20%20%20%20:target:%20https://pypi.python.org/pypi/pyroglancer)
+
+![image](https://github.com/SridharJagannathan/pyroglancer/raw/master/docs/_static/pyroglancer_logo.png)
+
+Pythonic interface to neuroglancer for displaying neuronal data like
+neurons, synapses, meshes (surfaces, volumes), annotations (points) in
+the precomputed format.
+
+Installation
+============
+
+The easiest way to install the package is via `pip`:
+
+    $ pip3 install git+git://github.com/SridharJagannathan/pyroglancer@master
+
+Documentation
+=============
+
+(COMING SOON!) <https://SridharJagannathan.github.io/pyroglancer>
+
+Features
+========
+
+> -   Fetch and display neurons from the hemibrain, FAFB dataset
+> -   Display synapse annotations etc
+
+Acknowledgements
+================
+
+Thanks to [Jeremy Maitin-Shepard](https://github.com/jbms) from google
+for inventing the precomputed format and answering many of my queries.
+
+Copyright & License
+===================
+
+Copyright (c) 2021, [Sridhar
+Jagannathan](https://github.com/SridharJagannathan). 3-clause BSD
+License.
--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from os import path
 from setuptools import setup, find_packages
-import sys
-import versioneer
+import re
 
+VERSIONFILE = "pyroglancer/__version__.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-# NOTE: This file must remain Python 2 compatible for the foreseeable future,
-# to ensure that we error out properly for people with outdated setuptools
-# and/or pip.
-min_version = (3, 6)
-if sys.version_info < min_version:
-    error = """
-pyroglancer does not support Python {0}.{1}.
-Python {2}.{3} and above is required. Check your Python version like so:
-
-python3 --version
-
-This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
-Upgrade pip like so:
-
-pip install --upgrade pip
-""".format(*(sys.version_info[:2] + min_version))
-    sys.exit(error)
 
 here = path.abspath(path.dirname(__file__))
 
@@ -35,14 +25,13 @@ with open(path.join(here, 'requirements.txt')) as requirements_file:
 
 setup(
     name='pyroglancer',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version=verstr,
     description="Pythonic interface to neuroglancer for displaying neuronal and synaptic data",
     long_description=readme,
     author="Sridhar Jagannathan",
     author_email='j.sridharrajan@gmail.com',
     url='https://github.com/SridharJagannathan/pyroglancer',
-    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
+    python_requires='>=3.6',
     packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
         'console_scripts': [
@@ -62,6 +51,14 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Natural Language :: English',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
