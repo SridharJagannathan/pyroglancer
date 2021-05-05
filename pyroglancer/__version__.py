acbb5b8ca76c33dd9d33203205fc7a2db791d7d2
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2021-05-05 23:14:49 +0100

    updated for line length

--- pyroglancer/__version__.py
+++ pyroglancer/__version__.py
@@ -1,4 +1,4 @@
-"""Package for co-visualisation of neuronal data like neurons, synapses, meshes (surfaces, volumes), annotations (points) in the precomputed format."""
+"""Package for co-visualisation of neuronal data in the precomputed format."""
 
 __version__ = "0.0.4"
 __version_vector__ = (0, 0, 4)
