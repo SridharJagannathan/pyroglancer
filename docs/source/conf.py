6034b30da4f2365bf08af8d05a9286138c0c4dd1
SridharJagannathan <j.sridharrajan@gmail.com>
2021-07-13 07:11:48 +0100

    updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,17 +3,15 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+sys.path.insert(0, os.path.abspath('../../pyroglancer'))
 import pyroglancer
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pyroglancer'))
+
 
 # -- Project information -----------------------------------------------------
 
