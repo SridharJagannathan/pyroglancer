b6263c08c4da836d652a1fa7d7d50eb7b49be10b
SridharJagannathan <j.sridharrajan@gmail.com>
2021-07-13 15:15:58 +0100

    bump version

--- pyroglancer/__version__.py
+++ pyroglancer/__version__.py
@@ -1,4 +1,4 @@
 """Package for co-visualisation of neuronal data in the precomputed format."""
 
-__version__ = "0.0.4"
-__version_vector__ = (0, 0, 4)
+__version__ = "0.0.5"
+__version_vector__ = (0, 0, 5)
