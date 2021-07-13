d64136290b630ee979dbe1143e93d1e8d3c7ce52
SridharJagannathan <j.sridharrajan@gmail.com>
2021-07-13 07:16:58 +0100

    fix import error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,11 +7,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../pyroglancer'))
-import pyroglancer
 import os
 import sys
-
+sys.path.insert(0, os.path.abspath('../../pyroglancer'))
+import pyroglancer
 
 # -- Project information -----------------------------------------------------
 
